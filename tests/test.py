6e3f6b898144ce89307e61858364d005f8f6a35d
Karel Blavka <karel.blavka@bigclown.com>
2021-09-22 12:02:59 +0200

    Add test for more complex payload

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 import unittest
 import io
 import sys
+import json
 from paho.mqtt.client import MQTTMessage
 from mqtt2influxdb.mqtt2influxdb import Mqtt2InfluxDB
 from mqtt2influxdb.config import load_config
@@ -24,6 +25,13 @@ points:
     tags:
       id: $.topic[1]
       channel: $.topic[3]
+
+  - measurement: tempnexus
+    topic: home/+/PilighttoMQTT/nexus/42/
+    fields:
+      value: $.payload.message.temperature
+    tags:
+      id: $.topic[1]
 '''
 
 
@@ -76,7 +84,7 @@ class TestTransform(unittest.TestCase):
 
     def test_measurement_temperature(self):
         self.mqtt_pub('node/push-button:1/thermometer/0:1/temperature', 22.94)
-        print(self.influxdb.data)
+        # print(self.influxdb.data)
         self.assertEqual(len(self.influxdb.data), 1, msg='Expect 1 call write_points')
         self.assertEqual(len(self.influxdb.data[0]['points']), 1, msg='Expect 1 points')
         self.assertEqual(self.influxdb.data[0]['database'], 'node')
@@ -84,6 +92,20 @@ class TestTransform(unittest.TestCase):
         self.assertEqual(point['measurement'], 'temperature')
         self.assertEqual(point['fields']['value'], 22.94)
         self.assertDictEqual(point['tags'], {'id': 'push-button:1', 'channel': '0:1'})
+        self.assertTrue('time' in point)
+
+    def test_measurement_tempnexus(self):
+        message = {"message": {"id": 42, "channel": 1, "battery": 1, "temperature": 12.9, "humidity": 0},
+                   "protocol": "nexus", "length": "42", "value": "42", "repeats": 2, "status": 2}
+        self.mqtt_pub('home/test/PilighttoMQTT/nexus/42/', json.dumps(message))
+        # print(self.influxdb.data)
+        self.assertEqual(len(self.influxdb.data), 1, msg='Expect 1 call write_points')
+        self.assertEqual(len(self.influxdb.data[0]['points']), 1, msg='Expect 1 points')
+        self.assertEqual(self.influxdb.data[0]['database'], 'node')
+        point = self.influxdb.data[0]['points'][0]
+        self.assertEqual(point['measurement'], 'tempnexus')
+        self.assertEqual(point['fields']['value'], 12.9)
+        self.assertDictEqual(point['tags'], {'id': 'test'})
         self.assertTrue('time' in point)
 
 
